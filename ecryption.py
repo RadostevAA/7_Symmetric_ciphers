0665b339f1eb0956ebaf7f1da6bee9317ca5285c
RadostevAA <55791524+RadostevAA@users.noreply.github.com>
2019-12-26 18:03:55 +0300

    Готово
    Выполнено

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,30 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+import random
 
-print(encrypt(2, 'Hello!'))
+def encrypt(k, m):
+    string = ''
+    for i in m:
+        string += chr(ord(i) + k)
+    return string
+
+def decrypt(k,m):
+    string = ''
+    for i in m:
+        string += chr(ord(i) - k)
+    return string
+
+def hack(msg):
+    s = {i: msg.count(i) for i in msg}
+    string = ''
+    k = ord(max(s, key=s.get)) - 108
+    for i in msg:
+        string += chr(ord(i) - k)
+    return string
+
+
+k = random.randint(1, 33)    
+msg = 'Hello, World.'
+msg1= encrypt(k, msg)
+print(msg1)
+msg2 = hack(msg1)
+print('\n')
+print(msg2)
